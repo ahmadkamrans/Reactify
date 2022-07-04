1a1c3d0fd4e21cfceb7c9e6938edfc29bd692ce3
Ahmad Kamran <ahmad.eritheia@gmail.com>
2022-07-05 02:17:20 +0500

    Update and Delete API Created

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,6 +1,10 @@
 const { Schema, default: mongoose } = require("mongoose");
 
 const NotesSchema = new Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'user'
+    },
     title : {
         type : String,
         required : true
